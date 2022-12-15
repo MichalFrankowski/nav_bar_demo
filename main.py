d92988a8b31f7943f14136c0a546b078f3b6e4c4
MichalFrankowski <michal.m.frankowski@gmail.com>
2022-12-15 20:33:45 +0000

    Adding button for testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.behaviors import ToggleButtonBehavior,ButtonBehavior
 from kivy.uix.label import Label
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
@@ -13,13 +13,16 @@ from kivy.properties import ObjectProperty
 
 #first set execution policy
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-#RUN kivy_venv\Scripts\activate.ps1 TO ACTIVATE VIRTUAL ENV
+#RUN kivy_venv\Scripts\activate.ps1 TO ACTIVATE VIRTUAL ENV 
 
 #main view of the app
 class navbardemo(BoxLayout,Screen):
-   manager = ObjectProperty(None) 
-   def __init__(self, **kwargs):
-       super().__init__(**kwargs)  
+    manager = ObjectProperty(None) 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  
+       
+    def get_file(self):
+        print("button clicked") 
 
 class ScreenManager(ScreenManager):
     pass
@@ -29,7 +32,6 @@ class FirstView(Screen):
 
 class SecondView(Screen):
     pass
-
 
 #custom navtab buttons
 class NavTab(ToggleButtonBehavior, BoxLayout):
@@ -48,7 +50,7 @@ class NavTab(ToggleButtonBehavior, BoxLayout):
     #hover animation for nav buttons
     def on_motion(self, src, mouse_pos):
         if self.collide_point(*mouse_pos):
-            print('over Button at', mouse_pos, 'Do animation')
+            #print('over Button at', mouse_pos, 'Do animation')
             self.background= (10/255, 65/255, 112/255,1)
         else:   
             self.background= (23/255, 23/255, 23/255,1) 
@@ -61,10 +63,12 @@ class Text(Label):
 class navbardemoApp(App):
     def build(self):
         return navbardemo()
-    
+   
     #custom font - CURRENTLY NOT USE AS USING STANDARD STYLE "VERDANA"
     LabelBase.register(name='Roboto-Black',
                    fn_regular='assets/fonts/Roboto/Roboto-Bold.ttf')
             
 if __name__ == '__main__':
     navbardemoApp().run()
+    
+      
--- views/firstview/FirstView.py
+++ views/firstview/FirstView.py
@@ -1,10 +1,10 @@
-from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('views/firstView/FirstView.kv')
 
-class FirstView(Screen, BoxLayout):
-   def __init__(self, **kwargs):
-       super().__init__(**kwargs)  
+class FirstView(BoxLayout):
+    def get_file(self):
+        print("button clicked") 
+       
+ 
